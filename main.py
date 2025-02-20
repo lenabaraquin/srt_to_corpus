32b5b4be911ebc92e5cc30e7ab77e34783cc3edd
Léna Baraquin <baraquin.lena@gmail.com>
2025-02-20 02:13:42 +0100

    ajout de fonctions

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import re
 
-with open("test.srt", encoding="iso-8859-15") as subtitle:
-    cleaned_text = ""
-    for line in subtitle:
-        line = line.replace("\n", "")
-        if "" == line:
-            continue
-        if re.match(r"^\d+", line):
-            continue
-        if "-->" in line:
-            continue
-        line = re.sub(r"</?\w+>", "", line)
-        cleaned_text += line
-        cleaned_text += "\n"
-    print(cleaned_text)
+
+def add_text(id: str) -> str:
+    with open("test.srt", encoding="iso-8859-15") as subtitle:
+        cleaned_text = ""
+        for line in subtitle:
+            line = line.replace("\n", "")
+            if "" == line:
+                continue
+            if re.match(r"^\d+", line):
+                continue
+            if "-->" in line:
+                continue
+            line = re.sub(r"</?\w+>", "", line)
+            cleaned_text += line
+            cleaned_text += "\n"
+        return cleaned_text
+
+
+def add_start_balise(id: str) -> str:
+    to_add = "<id = "
+    to_add += id
+    to_add += ">"
+    return to_add
+
+
+def add_end_balise() -> str:
+    to_add = "</id>"
+    return to_add
+
+
+def generate_corpus() -> str:
+    id = "test.srt"
+    corpus = ""
+    corpus += add_start_balise(id)
+    corpus += add_text(id)
+    corpus += add_end_balise()
+    return corpus
+
+
+corpus = generate_corpus()
+print(corpus)
